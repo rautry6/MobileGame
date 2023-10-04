c629acbaf5bbe0fffc428b593ccfd765864ae2a0
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2023-10-04 08:48:44 -0400

    Fix use item button, UI fixes, skyboxes to all minigames

--- Assets/Scripts/UI/MinigameFinish.cs
+++ Assets/Scripts/UI/MinigameFinish.cs
@@ -50,9 +50,9 @@ namespace UI
 
         public void BackToHomeWithPrize()
         {
+            
             SlimeCareStats.Instance.SetPrize(prizeToTakeBack);
             StartCoroutine(LoadSlimeCareRoutine());
-            
         }
 
         public void BackToHomeNoPrize()
